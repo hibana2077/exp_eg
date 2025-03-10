7b581cf34a4bf30a1c9b336d74172a70b8f53efc
hibana2077 <hibana2077@gmail.com>
2025-03-10 06:18:40 +0000

    Create database if it doesn't exist in save_vec_store function

--- src/core/utils/vec_store.py
+++ src/core/utils/vec_store.py
@@ -18,10 +18,13 @@ def text_transform(data:dict)->dict:
     }
 
 def save_vec_store(kb_name:str, file_name:str, data:dict):
+    # Connect to InfinityDB
     SERVER_IP_ADDRESS = os.getenv("INFINITY_HOST", "localhost")
     INFINITY_PORT = os.getenv("INFINITY_PORT", "23817")
     infinity_obj = infinity.connect(infinity.NetworkAddress(SERVER_IP_ADDRESS, INFINITY_PORT))
-    db_object = infinity_obj.get_database(kb_name.lower())
+    
+    # Create a database if it doesn't exist
+    db_object = infinity_obj.create_database(kb_name.lower())
     table_name = file_name.split(".")[0]
     texts_table = db_object.create_table(table_name, TEXT_FORMAT)
     for i in range(len(data['texts'])):texts_table.insert([text_transform(data['texts'][i])])
