960a88e130466155f7f0bf1c4f1fac6f124d3785
hibana2077 <hibana2077@gmail.com>
2025-03-10 03:32:29 +0000

    Refactor MinIO integration in kb.py: update environment variable names and enhance file upload functionality with MinIO client

--- src/web/st_comp/kb.py
+++ src/web/st_comp/kb.py
@@ -9,27 +9,41 @@ from minio import Minio
 from utils.size_cal import size_cal
 
 MINIO_SERVER = os.getenv("MINIO_SERVER", "minio:9000")
-MINIO_ROOT_USER = os.getenv("MINIO_ROOT_USER", "root")
-MINIO_ROOT_PASSWORD = os.getenv("MINIO_ROOT_PASSWORD", "password")
+MINIO_USER = os.getenv("MINIO_ROOT_USER", "root")
+MINIO_PASSWORD = os.getenv("MINIO_ROOT_PASSWORD", "password")
 
 @st.dialog("View Knowledge Base")
-def view_kb_dialog(name:str):
+def view_kb_dialog(kb_name:str):
     # Add view KB API
     tab1, tab2, tab3 = st.tabs(["Dataset","Retrieval testing","Configuration"])
     with tab1:
-        st.write(f"Viewing KB: {name}")
+        st.write(f"Viewing KB: {kb_name}")
         st.write("This is a dummy content")
         uploaded_files = st.file_uploader(
             "Upload files", accept_multiple_files=True
         )
         for uploaded_file in uploaded_files:
-            # bytes_data = uploaded_file.read()
-            # st.write("filename:", uploaded_file.name)
             st.markdown(f"- filename: {uploaded_file.name}, type: {uploaded_file.type}, size: {size_cal(uploaded_file.size)}")
-            # st.write(bytes_data)
         if len(uploaded_files) > 0:
             if st.button("Upload"):
-                pass
+                client = Minio(
+                    "minio:9000",
+                    access_key=MINIO_USER,
+                    secret_key=MINIO_PASSWORD,
+                    secure=False,
+                )
+                # result = client.fput_object(
+                #     "my-bucket", "my-object", "my-filename",
+                # )
+                # save file to local temp
+                for uploaded_file in uploaded_files:
+                    with open(uploaded_file.name, "wb") as f:
+                        f.write(uploaded_file.getbuffer())
+                    # Upload to Minio
+                    client.fput_object(
+                        kb_name, uploaded_file.name, uploaded_file.name,
+                    )
+                    os.remove(uploaded_file.name)
 
     with tab2:
         st.write("Retrieval testing")
