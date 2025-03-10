4ca6405b935b32c2019ad08d37cffecf5768e90e
hibana2077 <hibana2077@gmail.com>
2025-03-10 05:56:30 +0000

    Update save_vec_store function to accept file_name parameter for data storage

--- src/core/utils/vec_store.py
+++ src/core/utils/vec_store.py
@@ -15,7 +15,7 @@ def text_transform(data:dict)->dict:
          "text": data["text"]
     }
 
-def save_vec_store(kb_name:str, data:dict):
+def save_vec_store(kb_name:str, file_name:str, data:dict):
     SERVER_IP_ADDRESS = os.getenv("INFINITY_HOST", "localhost")
     INFINITY_PORT = os.getenv("INFINITY_PORT", "23817")
     infinity_obj = infinity.connect(infinity.NetworkAddress(SERVER_IP_ADDRESS, INFINITY_PORT))
