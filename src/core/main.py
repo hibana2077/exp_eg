3c921e69f21a4c927d62911edd62fb6c7cea71ca
hibana2077 <hibana2077@gmail.com>
2025-03-10 05:21:35 +0000

    Remove unused imports from main.py to clean up the codebase

--- src/core/main.py
+++ src/core/main.py
@@ -7,19 +7,6 @@ from minio import Minio
 from fastapi import FastAPI, HTTPException
 from pathlib import Path
 
-import tesserocr
-from docling.backend.pypdfium2_backend import PyPdfiumDocumentBackend
-from docling.datamodel.base_models import FigureElement, InputFormat, Table
-from docling.datamodel.pipeline_options import (
-    AcceleratorDevice,
-    AcceleratorOptions,
-    PdfPipelineOptions,
-)
-from docling.document_converter import DocumentConverter, PdfFormatOption
-from docling.models.tesseract_ocr_model import TesseractOcrOptions
-from docling_core.types.doc import ImageRefMode, PictureItem, TableItem
-
-
 HOST = os.getenv("HOST", "127.0.0.1")
 MINIO_USER = os.getenv("MINIO_ROOT_USER", "root")
 MINIO_PASSWORD = os.getenv("MINIO_ROOT_PASSWORD", "password")
