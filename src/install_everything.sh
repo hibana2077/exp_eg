923e19b905ca2b0a00722b667b9d9b2fc788a1bf
hibana2077 <hibana2077@gmail.com>
2025-03-10 01:42:14 +0000

    Update install script to install Docker Compose from GitHub releases and configure Git user

--- src/install_everything.sh
+++ src/install_everything.sh
@@ -1,3 +1,8 @@
 apt update
 curl -sSL https://get.docker.com | sh
-apt install btop docker-compose python3-pip -y
+apt install btop python3-pip -y
+git config --global user.email "hibana2077@gmail.com" && git config --global user.name "hibana2077"
+VERSION=$(curl --silent https://api.github.com/repos/docker/compose/releases/latest | grep -Po '"tag_name": "\K.*\d')
+DESTINATION=/usr/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/${VERSION}/docker-compose-$(uname -s)-$(uname -m) -o $DESTINATION
+sudo chmod 755 $DESTINATION
