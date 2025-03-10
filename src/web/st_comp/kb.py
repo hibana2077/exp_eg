1d003ff14ce1ae025efaa051d3ac6e2f822110ab
hibana2077 <hibana2077@gmail.com>
2025-03-10 03:21:51 +0000

    Add MinIO configuration to docker-compose and update kb.py for environment variables

--- src/web/st_comp/kb.py
+++ src/web/st_comp/kb.py
@@ -8,6 +8,10 @@ from minio import Minio
 # Self-defined imports
 from utils.size_cal import size_cal
 
+MINIO_SERVER = os.getenv("MINIO_SERVER", "minio:9000")
+MINIO_ROOT_USER = os.getenv("MINIO_ROOT_USER", "root")
+MINIO_ROOT_PASSWORD = os.getenv("MINIO_ROOT_PASSWORD", "password")
+
 @st.dialog("View Knowledge Base")
 def view_kb_dialog(name:str):
     # Add view KB API
