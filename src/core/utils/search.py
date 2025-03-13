0443d93813ccd3bc1c14c237cd8944b4e8430b3e
hibana2077 <hibana2077@gmaill.com>
2025-03-13 16:25:46 +0800

    Add query parameter to search function in search.py

--- src/core/utils/search.py
+++ src/core/utils/search.py
@@ -4,6 +4,7 @@ import os
 
 def search(db_name:str,
            tables:list[str],
-           select_col:list[str]):
+           select_col:list[str],
+           query:str):
     """"""
     return None
