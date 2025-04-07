c361f504446901513e89baec7e4e7e86b3243a5b
hibana2077 <hibana2077@gmaill.com>
2025-04-07 14:52:04 +0800

    Update image embedding search metric to use cosine similarity

--- src/core/cfg/emb_settings.py
+++ src/core/cfg/emb_settings.py
@@ -2,5 +2,5 @@ EMB_MODEL = "intfloat/multilingual-e5-large"
 EMB_SEARCH_METRIC = "cosine" # ip -> inner product, cosine -> cosine similarity, l2 -> l2 distance
 IMG_EMB_MODEL = "Qdrant/clip-ViT-B-32-vision"
 IMG_CLIP_EMB_MODEL = "Qdrant/clip-ViT-B-32-text"
-IMG_EMB_SEARCH_METRIC = "ip"
+IMG_EMB_SEARCH_METRIC = "cosine"
 EMB_DIM = 1024
