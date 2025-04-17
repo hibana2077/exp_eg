50332977c007810b3a3649dfd8c235b0cd69e7db
hibana2077 <hibana2077@gmaill.com>
2025-04-17 11:06:01 +0800

    Refactor code structure for improved readability and maintainability

--- lab/lab.py
+++ lab/lab.py
@@ -1,15 +1,28 @@
 import pandas as pd
 
-df = pd.DataFrame({
-    "text": ["This is a sample text.", "Another sample text."],
-    "label": ["label1", "label2"],
-    "prov": [{"page_no": 1, "bbox": {"x0": 0, "y0": 0, "x1": 100, "y1": 100}}, {"page_no": 2, "bbox": {"x0": 10, "y0": 10, "x1": 110, "y1": 110}}],
-    "self_ref": ["ref1", "ref2"],
-    "parent": [{"$ref": "parent_ref1"}, {"$ref": "parent_ref2"}],
-    "content_layer": ["layer1", "layer2"],
-    "orig": ["original text 1", "original text 2"]
-})
+org_acc_df = pd.read_csv('./results-imagenet.csv')
+prof_df = pd.read_csv('./benchmark-infer-amp-nchw-pt113-cu117-rtx3090.csv')
 
-print(df)
-# drop index
-df.reset_index(drop=True, inplace=True)
+acc_df = org_acc_df.copy()
+acc_df['pretrained'] = acc_df['model'].str.split('.').str[1]
+acc_df['model'] = acc_df['model'].str.split('.').str[0]
+
+# merge
+merged_df = pd.merge(acc_df, prof_df, on='model', how='inner')
+# efficiency metric
+merged_df['efficiency'] = merged_df['top1'] / merged_df['infer_gmacs']
+# sort by [efficiency]
+merged_df = merged_df.sort_values(by=['efficiency'], ascending=False)
+# reset index
+merged_df = merged_df.reset_index(drop=True)
+# print
+print(merged_df.head(20))
+
+print(merged_df.loc[merged_df['model'] == 'vit_base_patch32_224', ['model', 'pretrained', 'top1', 'infer_gmacs']], end='\n\n') # 4.41
+print(merged_df.loc[merged_df['model'] == 'vit_base_patch16_224', ['model', 'pretrained', 'top1', 'infer_gmacs']], end='\n\n') # 17.58
+print(merged_df.loc[merged_df['model'] == 'vit_huge_patch14_clip_224', ['model', 'pretrained', 'top1', 'infer_gmacs']], end='\n\n') # 167.4
+print(merged_df.loc[(merged_df['infer_gmacs'] < 4.41) & (merged_df['infer_gmacs'] > 1), ['model', 'pretrained', 'top1', 'infer_gmacs']].sort_values(by=['top1'], ascending=False).head(5), end='\n\n')
+print(merged_df.loc[(merged_df['infer_gmacs'] < 17.58) & (merged_df['infer_gmacs'] > 4.41), ['model', 'pretrained', 'top1', 'infer_gmacs']].sort_values(by=['top1'], ascending=False).head(5), end='\n\n')
+print(merged_df.loc[(merged_df['infer_gmacs'] < 167.4) & (merged_df['infer_gmacs'] > 17.58), ['model', 'pretrained', 'top1', 'infer_gmacs']].sort_values(by=['top1'], ascending=False).head(5), end='\n\n')
+
+print(merged_df.loc[(merged_df['model'].str.startswith("deit")), ['model', 'pretrained', 'top1', 'infer_gmacs']].sort_values(by=['top1'], ascending=False).head(5), end='\n\n')
