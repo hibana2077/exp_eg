52c41b86182269fca2d8a44f97b293d7af9aa121
hibana2077 <hibana2077@gmail.com>
2025-06-25 10:01:52 +0800

    refactor: Refactor code structure for improved readability and maintainability

--- src/langflow/components/list_all_file.py
+++
@@ -1,9 +0,0 @@
-import io
-import requests
-from langflow.base.data import BaseFileComponent
-from langflow.base.data.utils import TEXT_FILE_TYPES, parallel_load_data, parse_text_file_to_data
-from langflow.io import BoolInput, IntInput, StrInput, SecretStrInput
-from langflow.schema import Data
-
-
-class ListAllFilesComponent(BaseFileComponent):
